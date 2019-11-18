e20d3713201b72bc024cfa9cc614ecb63a0fc23a
s-yib <s-yib@53S500110834.it.bsd405.org>
2019-11-18 08:12:44 -0800

    Turning in final checkpoint (updated further)

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -11,9 +11,13 @@ public class FracCalc {
 		// TODO: Read the input from the user and call produceAnswer with an equation
 		Scanner sc = new Scanner(System.in);
 		String equation = "";
+		System.out.println("Fraction Calculator");
+		System.out.println("Fraction format: whole_numerator/denominator (Ex: 1_1/2)");
+		System.out.println("Operators: +, -, *, /\n");
 		while (equation != "quit") {
+			System.out.println("Values to calculate?\n");
 			equation = sc.nextLine();
-			System.out.println(produceAnswer(equation));
+			System.out.println(produceAnswer(equation)+"\n");
 		}
 		sc.close();
 	}
